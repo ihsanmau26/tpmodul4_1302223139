0c2f6660404a5a2d1cb9518377f2c039d614c084
Ihsan Maulana <96187706+ihsanmau26@users.noreply.github.com>
2024-03-13 04:15:50 +0700

    Add files via upload
    menambahkan implementasi dengan table-driven

--- tpmodul4_1302223139/tpmodul4_1302223139/Program.cs
+++ tpmodul4_1302223139/tpmodul4_1302223139/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class KodePos
+{
+    private Dictionary<string, string> kodePos;
+
+    public KodePos()
+    {
+        kodePos = new Dictionary<string, string>
+        {
+            {"Batununggal", "40266"},
+            {"Kujangsari", "40287"},
+            {"Mengger", "40267"},
+            {"Wates", "40256"},
+            {"Cijaura", "40287"},
+            {"Jatisari", "40286"},
+            {"Margasari", "40286"},
+            {"Sekejati", "40286"},
+            {"Kebonwaru", "40272"},
+            {"Maleer", "40274"},
+            {"Samoja", "40273"}
+        };
+    }
+
+    public string GetKodePos(string kelurahan)
+    {
+        if (kodePos.ContainsKey(kelurahan))
+        {
+            return kodePos[kelurahan];
+        }
+        else
+        {
+            return "Kode Pos Tidak Ditemukan";
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kodePosObj = new KodePos();
+        string kelurahan = "Batununggal";
+        string kodePos = kodePosObj.GetKodePos(kelurahan);
+        Console.WriteLine($"Kode Pos {kelurahan}: {kodePos}");
+
+        kelurahan = "Kujangsari";
+        kodePos = kodePosObj.GetKodePos(kelurahan);
+        Console.WriteLine($"Kode Pos {kelurahan}: {kodePos}");
+
+        kelurahan = "Cijaura";
+        kodePos = kodePosObj.GetKodePos(kelurahan);
+        Console.WriteLine($"Kode Pos {kelurahan}: {kodePos}");
+    }
+}
